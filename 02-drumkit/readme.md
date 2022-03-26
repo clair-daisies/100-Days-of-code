e35ce2c95c3c24192b9c3df7b2c5fbfa983699fb
delfina <delfina.ghimire@gmail.com>
2022-03-26 15:08:19 +0545

    JS DrumKit

---
+++ 02-drumkit/readme.md
@@ -0,0 +1,11 @@
+# Day 2 : Drum Kit
+
+### Concepts Used
+
+- 'Data- ' attribute
+- Attribute Selector
+- play()
+- Event listener - animationend
+- arrow function
+- forEach loop
+- add and remove class
---
+++ 02-drumkit/script.js
@@ -0,0 +1,19 @@
+let playSound = function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.play();
+  audio.currentTime = 0;
+  key.classList.add("playing");
+};
+window.addEventListener("keydown", playSound);
+
+let removeTransition = function (e) {
+  if (e.propertyName != "transform") return; //skip if not transform
+  this.classList.remove("playing");
+};
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+});
